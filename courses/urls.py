df957577c2eb7d128af3adfc540d508ba034a245
halil <halil@DESKTOP-38P5TGT>
2023-06-13 03:49:50 +0300

    added dynamic data with for loop

--- courses/urls.py
+++ courses/urls.py
@@ -5,10 +5,6 @@ from courses import views
 
 urlpatterns = [
     path('',views.index),
-    path('liste',views.courses),
-    path('detaylar',views.details),
-    # path('programlama',views.programming), dinamik hale getirerek metod tanımlamasından kurtulduk
-    # path('mobil-uygulamalar',views.mobile_app), bir alt satırdaki yaptığımız gibi
     path('kategoriler/<int:category_id>',views.category_by_id),
     path('kategoriler/<str:category>',views.get_courses_by_cname,name='category_by_id'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -7,28 +7,14 @@ data={
     "mobil":"mobil programlama detayları",
     "masaüstü":"masaüstü programlama detayları",
     "gömülü-sistemler":"gömülü sistemler detayları",
+    "django":"django detayları",
+    ".net":".net detayları",
 }
 
-def index(request):
-    return render(request,'courses/index.html')
-
-
 # Create your views here.
-def courses(request):
-    list_items = ""
-    for category in list(data.keys()):
-        redirect_url = reverse('category_by_id',args=[category])
-        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>" 
-    return HttpResponse(f"<ul>{list_items}</ul>")
+def index(request):
+    return render(request,'courses/courses.html',{'data':data.keys})
 
-
-
-def details(request):
-    return HttpResponse("detaylar sayfası")
-# def programming(request):
-#     return HttpResponse("programlama sayfası")
-# def mobile_app(request):
-#     return HttpResponse("mobil uygulama sayfası")
 def get_courses_by_cname(request,category):
     
     try:
