cccc7fa020d8d021a6de8836fe22bda76e5b3162
halil <halil@DESKTOP-38P5TGT>
2023-06-16 18:09:16 +0300

    updated filter by category in courses

--- courses/urls.py
+++ courses/urls.py
@@ -5,7 +5,6 @@ from courses import views
 
 urlpatterns = [
     path('',views.index),
-    path('kategoriler/<int:category_id>',views.get_category_by_id,name="category_id"),
     path('<slug:slug>',views.course_detail,name="detail"),
-    path('kurslar/',views.index),
+    path('kategoriler/<slug:slug>',views.get_course_by_category,name="course_by_category"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -7,19 +7,17 @@ from .models import Course,Category
 
 
 def course_detail(request,slug):
-
-    
     return render(request,'courses/detail.html',{'course':get_object_or_404(Course,slug=slug)})
     
     
-
-
 def index(request):
 
     return render(request,'courses/index.html',{'categories':Category.objects.all(),"courses":Course.objects.all()})
 
-def get_category_by_id(request,category_id):
-    
-    # return HttpResponse(filtered_data)
-    return render(request, 'courses/partials/_courses.html')
+def get_course_by_category(request,slug):
+    return render(request, 'courses/index.html',{
+        "courses": Course.objects.filter(category__slug=slug,isActive=True),
+        "categories": Category.objects.all(),
+        "selected":slug
+    })
     
